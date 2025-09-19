1f450ab96fa9f159844f08aa71c9f27251280da1
mananuf <bmananaf@gmail.com>
2025-09-19 16:11:27 +0100

    chore: adds file for TaxTypes

---
+++ proxy-contract/contracts/lib/TaxTypes.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+enum TaxCategory { WHT, PAYE, VAT, CONSUMPTION, OTHER }
+enum IdType { BVN, NIN, CAC, VOTERS_CARD, DRIVERS_LICENSE, TIN }
+enum Gender { MALE, FEMALE }
